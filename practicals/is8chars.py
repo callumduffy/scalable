daa670cbafc84afe64fd188df51b183f42573d2d
Callum Duffy <duffyc8@tcd.ie>
2018-10-01 20:07:04 +0100

    new dict file

--- practicals/is8chars.py
+++ practicals/is8chars.py
@@ -1,10 +1,10 @@
 cracked = False
 
-with open("words.txt", "r") as u:
+with open("1.6millpwords.txt", "r") as u:
 	words=u.readlines()
 	words=  [x.strip() for x in words]
 	
-	with open("8CharWords.txt","a") as new:
+	with open("8CharPwords.txt","a") as new:
 		
 		for w in words:
 			if len(w) == 8:
