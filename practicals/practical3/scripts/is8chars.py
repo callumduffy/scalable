41e412ac0c571620b07265fc7d6e806efd9eb5a7
Callum Duffy <duffyc8@tcd.ie>
2018-10-02 14:41:27 +0100

    adding structure to files

--- practicals/practical3/scripts/is8chars.py
+++ practicals/practical3/scripts/is8chars.py
@@ -1,10 +1,6 @@
 with open("2BillPwords.txt", "r", errors="ignore") as us:
 	words=us.readlines()
-	words=  [x.strip() for x in words]
-	
+	eight_char_words = [word for word in words if len(word) == 9]
 	with open("8Char2BillPwords.txt","w") as new:
-		
-		for w in words:
-			if len(w) == 8:
-				w=w+"\n"
-				new.write(w)
+		for word in eight_char_words:
+			new.write("%s" % word)
--- practicals/practical3/scripts/replaceColon.py
+++ practicals/practical3/scripts/replaceColon.py
@@ -1,10 +1,9 @@
 with open("duffyc8.cracked", "r") as colon:
 	lines=colon.readlines()
+	print(len(lines))
 	
 	with open("duffyc8.broken", "a") as final:
-	
-	for l in lines:
-		l.replace(";"," ")
-		l=l+"\h"
-		final.write(l)
-		
+		for l in lines:
+			l = l.replace(":"," ")
+			l = l.replace('\r','')
+			final.write(l)
