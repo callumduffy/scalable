b6925ce5da4c64892bee00c1edab8a95a3de00a6
Callum Duffy <duffyc8@tcd.ie>
2018-10-13 16:53:38 +0100

    fix sha file

--- practicals/practical4/scripts/ismd5.py
+++ practicals/practical4/scripts/ismd5.py
@@ -1,6 +1,6 @@
 with open("duffyc8.hashes", "r", errors="ignore") as us:
 	words=us.readlines()
 	eight_char_words = [word for word in words if len(word) == 64]
-	with open("sha256cryptHashes.hashes","w") as new:
+	with open("sha256.hashes","w") as new:
 		for word in eight_char_words:
 			new.write("%s" % word)
