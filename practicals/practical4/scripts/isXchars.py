543d7eca56fa968da1f29efaf72d04f628b74b69
duffyc8 <duffyc8@tcd.ie>
2018-10-16 17:58:26 +0100

    some md5 hashes done

--- practicals/practical4/scripts/isXchars.py
+++ practicals/practical4/scripts/isXchars.py
@@ -1,6 +1,6 @@
-with open("1.6millpwords.txt", "r", errors="ignore") as us:
+with open("duffyc8.hashes", "r", errors="ignore") as us:
 	words=us.readlines()
-	eight_char_words = [word for word in words if len(word) == 6]
-	with open("5letter.txt","w") as new:
+	eight_char_words = [word for word in words if len(word) == 35]
+	with open("md5.hashes","w") as new:
 		for word in eight_char_words:
 			new.write("%s" % word)
